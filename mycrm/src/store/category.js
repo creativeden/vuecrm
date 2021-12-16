2fa88da6ea88e9dcc7c9ae3d34c1e217bd511192
creativeden <info@creativeden.ru>
2021-12-16 23:12:50 +0200

    Start Lesson 11

--- mycrm/src/store/category.js
+++ mycrm/src/store/category.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, push, child, update } from "firebase/database";
+import { getDatabase, ref, get, push, child, update, onValue } from "firebase/database";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBRRlYl1k2q8ycTU3Y3UdmIkWs4_hd3lcE",
@@ -16,6 +16,48 @@ const database = getDatabase(app);
 
 export default {
     actions: {
+        async fetchCategories({ dispatch, commit }) {
+            try {
+                const uid = await dispatch('getUid')
+
+                // const dbRef = ref(getDatabase())
+                
+                // const categories = await get(child(dbRef, `users/${uid}/categories`)).then((snapshot) => {
+                //     if (snapshot.exists()) {
+                //         console.log(snapshot.val())
+                //         console.log(categories)
+                //     } else {
+                //         console.log("No data available")
+                //     }
+                // })
+
+                const categories = ref(database, `users/${uid}/categories`)
+                onValue(categories, (snapshot) => {
+                    const data = snapshot.val()
+                    // console.log(snapshot.val())
+                    // return data
+                    // updateStarCount(postElement, data)
+                    const cats = []
+                    Object.keys(categories).forEach(key => {
+                        cats.push({
+                            title: categories[key].title,
+                            limit: categories[key].limit,
+                            id: key
+                        })
+                    })
+                    console.log(data)
+                    return data
+                })
+
+                // console.log(Object.keys(categories))
+
+                
+                
+            } catch (e) {
+                commit('setError', e)
+                throw e
+            }
+        },
         async createCategory({ dispatch, commit }, { title, limit }) {
             try {
                 const uid = await dispatch('getUid')
